e87963b3a80380af38168ece41531c18cf64aee5
7ubi <tobi.renner03@gmail.com>
2021-05-10 15:10:50 +0200

    block placing

--- Assets/Scripts/PlayerActionController.cs
+++ Assets/Scripts/PlayerActionController.cs
@@ -6,15 +6,27 @@ using UnityEngine.UIElements;
 public class PlayerActionController : MonoBehaviour
 {
     [SerializeField] private float Range;
-    [SerializeField] private world_creation _worldCreation;
+    [SerializeField] private worldCreation _worldCreation;
     
     private void Update()
     {
-        if (!Input.GetMouseButtonDown(0)) return;
-        RaycastHit hit;
-        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Range))
-        {
-            _worldCreation.DestroyBlock(hit.point  + transform.forward * .01f, transform.position);
+        var leftClick = Input.GetMouseButtonDown(0);
+        var rightClick = Input.GetMouseButtonDown(1);
+        
+        
+        
+        if (leftClick || rightClick)
+        {   
+            RaycastHit hit;
+            if (!Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Range)) return;
+            if (leftClick)
+            {
+                _worldCreation.DestroyBlock(hit.point + transform.forward * .01f);
+            }
+            if(rightClick)
+            {
+                _worldCreation.PlaceBlock(hit.point + transform.forward * -.01f);            
+            }
         }
     }
 }
